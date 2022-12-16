683fae5c096060f85de5fba77a35fdd19e81455d
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-12-16 23:55:05 +0000

    triangle and interactive background

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     clear_color: wgpu::Color,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -56,6 +57,9 @@ impl State {
             .await
             .unwrap();
 
+        
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));  
+
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: surface.get_supported_formats(&adapter)[0],
@@ -64,6 +68,52 @@ impl State {
             present_mode: wgpu::PresentMode::Fifo,
             alpha_mode: wgpu::CompositeAlphaMode::Auto,
         };
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[], // 2.
+            },
+            fragment: Some(wgpu::FragmentState { // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState { // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1, // 2.
+                mask: !0, // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
+
         surface.configure(&device, &config);
 
         Self {
@@ -73,6 +123,7 @@ impl State {
             config,
             size,
             clear_color,
+            render_pipeline,
         }
     }
 
@@ -87,10 +138,10 @@ impl State {
 
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
-            WindowEvent::CursorMoved { 
-                device_id, 
-                position, 
-                modifiers 
+            WindowEvent::CursorMoved {
+                device_id,
+                position,
+                modifiers,
             } => {
                 let r = position.x / self.size.width as f64;
                 let g = position.y / self.size.height as f64;
@@ -100,9 +151,9 @@ impl State {
                     b: 0.3,
                     a: 1.0,
                 }
-            },
+            }
 
-            _ => {},
+            _ => {}
         }
 
         false
@@ -114,35 +165,39 @@ impl State {
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
-        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("Render Encoder"),
-        });
-    
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(
-                            self.clear_color,
-                        ),
+                        load: wgpu::LoadOp::Clear(self.clear_color),
                         store: true,
                     },
                 })],
                 depth_stencil_attachment: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
-    
+
         // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
-    
+
         Ok(())
     }
-
 }
 
 pub async fn run() {
@@ -181,24 +236,22 @@ pub async fn run() {
 
                         WindowEvent::Resized(physical_size) => {
                             state.resize(*physical_size);
-                        },
+                        }
 
                         WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
                             // new_inner_size is &&mut so we have to dereference it twice
                             state.resize(**new_inner_size);
-                        },
+                        }
 
-
-
-                        _ => {},
+                        _ => {}
                     }
                 }
-            },
+            }
 
             Event::RedrawRequested(window_id) if window_id == window.id() => {
                 state.update();
                 match state.render() {
-                    Ok(_) => {},
+                    Ok(_) => {}
                     // reconfig surface if lost
                     Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
                     // system out of memory -> quit
@@ -206,15 +259,15 @@ pub async fn run() {
                     // all other errors to be resoled by next frame
                     Err(e) => eprintln!("{:?}", e),
                 }
-            },
+            }
 
             Event::MainEventsCleared => {
                 // RedrawRequested will only trigger once, unless we manually
                 // request it.
                 window.request_redraw();
-            },
+            }
 
-            _ => {},
+            _ => {}
         }
     });
 }
