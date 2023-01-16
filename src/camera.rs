b52f43059a25e3154957ee85e5d50252b6891ec9
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2023-01-16 02:56:51 +0000

    better camera - slightly glitchy

--- src/camera.rs
+++ src/camera.rs
@@ -32,15 +32,15 @@ impl Camera {
         pitch: P,
     ) -> Self {
         Self {
-            position.into(),
-            yaw.into(),
-            pitch.into(),
+            position: position.into(),
+            yaw: yaw.into(),
+            pitch: pitch.into(),
         }
     }
 
     pub fn calc_matrix(&self) -> Matrix4<f32> {
         let (sin_pitch, cos_pitch) = self.pitch.0.sin_cos();
-        let (sin_yaw, cos_yaw) = sself.yaw.0.sin_cos();
+        let (sin_yaw, cos_yaw) = self.yaw.0.sin_cos();
 
         Matrix4::look_to_rh(
             self.position,
@@ -73,13 +73,17 @@ impl Projection {
     ) -> Self {
         Self {
             aspect: width as f32 / height as f32,
-            fovy.into(),
+            fovy: fovy.into(),
             znear,
             zfar,
         }
     }
 
-    pub fn resize(&mut self, width: u32, height: u32) -> Matrix4<f32> {
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.aspect = width as f32 / height as f32;
+    }
+
+    pub fn calc_matrix(&self) -> Matrix4<f32> {
         OPENGL_TO_WGPU_MATRIX * perspective(
             self.fovy, 
             self.aspect, 
@@ -162,10 +166,70 @@ impl CameraController {
         }
     }
 
-    fn process_mouse(&mut self, mouse_dx: f64, mouse_dy: f64) {
+    pub fn process_mouse(&mut self, mouse_dx: f64, mouse_dy: f64) {
         self.rotate_horizontal = mouse_dx as f32;
         self.rotate_vertical = mouse_dy as f32;
     }
+
+    pub fn process_scroll(&mut self, delta: &MouseScrollDelta) {
+        self.scroll = -match delta {
+            // assuming a line is 100 pixels
+            MouseScrollDelta::LineDelta(_, scroll) => scroll * 100.0,
+            MouseScrollDelta::PixelDelta(PhysicalPosition {
+                y: scroll,
+                ..
+            }) => *scroll as f32,
+        };
+    }
+
+    pub fn update_camera(&mut self, camera: &mut Camera, dt: Duration) {
+        let dt = dt.as_secs_f32();
+
+        // move forward/backward and left/right
+        let (yaw_sin, yaw_cos) = camera.yaw.0.sin_cos();
+        let forward = Vector3::new(yaw_cos, 0.0, yaw_sin).normalize();
+        let right = Vector3::new(-yaw_sin, 0.0, yaw_cos).normalize();
+        camera.position += forward * (
+            self.amount_forward - self.amount_backward
+        ) * self.speed * dt;
+        camera.position += right * (
+            self.amount_right - self.amount_left
+        ) * self.speed * dt;
+        
+        // move in/out (zoom)
+        let (pitch_sin, pitch_cos) = camera.pitch.0.sin_cos();
+        let scrollward = Vector3::new(
+            pitch_cos * yaw_cos, 
+            pitch_sin, 
+            pitch_cos * yaw_sin
+        ).normalize();
+        camera.position += scrollward * self.scroll * self.speed * self.sensitivity * dt;
+        self.scroll = 0.0;
+
+        // move up/down
+        // since we dont use roll, we can just modify the y-coordinate directly
+        camera.position.y += (
+            self.amount_up - self.amount_down
+        ) * self.speed * dt;
+
+        // rotate
+        camera.yaw += Rad(self.rotate_horizontal) * self.sensitivity * dt;
+        camera.pitch += Rad(-self.rotate_vertical) * self.sensitivity * dt;
+
+        // if process_mouse is not called each frame then these values will not
+        // be reset to zero, and the camera will rotate when moving in a  
+        // non-cardinal direction.
+        self.rotate_horizontal = 0.0;
+        self.rotate_vertical = 0.0;
+
+        // stop the cameras angle from going to high/low
+        if camera.pitch < -Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = -Rad(SAFE_FRAC_PI_2);
+        } else if camera.pitch > Rad(SAFE_FRAC_PI_2) {
+            camera.pitch = Rad(SAFE_FRAC_PI_2);
+        }
+    }
+
 }
 
 
